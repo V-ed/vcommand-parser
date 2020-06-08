0c30436e25f73aca8c78b5927ba4d456ff1df18c
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2020-06-08 11:39:13 -0400

    Update tests to new class names

--- tests/vcommander.test.ts
+++ tests/vcommander.test.ts
@@ -1,27 +1,27 @@
-import { VCommander } from '../src/vcommander';
+import { VCommandParser } from '../src/vcommandparser';
 
 describe('Basic Message Parsing', () => {
 	it('should return correct command', () => {
-		const testCommander = new VCommander('!mycommand');
+		const testCommander = new VCommandParser('!mycommand');
 		
 		expect(testCommander.command).toBe('mycommand');
 	});
 	
 	it('should return correct command and content', () => {
-		const testCommander = new VCommander('!mycommand my content');
+		const testCommander = new VCommandParser('!mycommand my content');
 		
 		expect(testCommander.command).toBe('mycommand');
 		expect(testCommander.content).toBe('my content');
 	});
 	
 	it('should return correct command with custom prefix', () => {
-		const testCommander = new VCommander('\\mycommand', '\\');
+		const testCommander = new VCommandParser('\\mycommand', '\\');
 		
 		expect(testCommander.command).toBe('mycommand');
 	});
 	
 	it('should return correct command and content with custom prefix', () => {
-		const testCommander = new VCommander('\\mycommand my content', '\\');
+		const testCommander = new VCommandParser('\\mycommand my content', '\\');
 		
 		expect(testCommander.command).toBe('mycommand');
 		expect(testCommander.content).toBe('my content');
