1596164f30adf73d1db9415b59f66ec5ba224903
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2020-06-07 21:45:58 -0400

    Add tests to confirm behaviour of basic parsing

--- tests/vcommander.test.ts
+++ tests/vcommander.test.ts
@@ -1,9 +1,29 @@
 import { VCommander } from '../src/vcommander';
 
-describe('Temporary testing', () => {
-	it('should return test', () => {
-		const testCommander = new VCommander('\\thx', '\\');
+describe('Basic Message Parsing', () => {
+	it('should return correct command', () => {
+		const testCommander = new VCommander('!mycommand');
 		
-		expect(testCommander.command).toBe('thx');
+		expect(testCommander.command).toBe('mycommand');
+	});
+	
+	it('should return correct command and content', () => {
+		const testCommander = new VCommander('!mycommand my content');
+		
+		expect(testCommander.command).toBe('mycommand');
+		expect(testCommander.content).toBe('my content');
+	});
+	
+	it('should return correct command with custom prefix', () => {
+		const testCommander = new VCommander('\\mycommand', '\\');
+		
+		expect(testCommander.command).toBe('mycommand');
+	});
+	
+	it('should return correct command and content with custom prefix', () => {
+		const testCommander = new VCommander('\\mycommand my content', '\\');
+		
+		expect(testCommander.command).toBe('mycommand');
+		expect(testCommander.content).toBe('my content');
 	});
 });
