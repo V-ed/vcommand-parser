d3643243917b134c863bfb2edcd91f168e12225c
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2020-06-09 14:33:03 -0400

    Add option prefix to vcommandparser class

---
+++ src/@types/OptionPrefix.ts
@@ -0,0 +1,3 @@
+type OptionPrefix = '-' | '~' | '=' | '+';
+
+export default OptionPrefix;
--- src/vcommandparser.ts
+++ src/vcommandparser.ts
@@ -1,16 +1,26 @@
+import OptionPrefix from './@types/OptionPrefix';
 import parseMessage from './message-parser';
 
 export class VCommandParser {
 	static readonly DEFAULT_COMMAND_PREFIX = '!';
+	static readonly DEFAULT_OPTION_PREFIX = '-';
 	
 	readonly message: string;
 	
 	readonly command?: string;
 	readonly content?: string;
 	
-	constructor(message: string, prefix = VCommandParser.DEFAULT_COMMAND_PREFIX) {
+	readonly prefix: string;
+	readonly optionPrefix: OptionPrefix;
+	
+	constructor(message: string, prefix = VCommandParser.DEFAULT_COMMAND_PREFIX, optionPrefix: OptionPrefix = VCommandParser.DEFAULT_OPTION_PREFIX) {
 		this.message = message;
+		this.prefix = prefix;
+		this.optionPrefix = optionPrefix;
 		
-		({command: this.command, content: this.content} = parseMessage(this.message, prefix));
+		({
+			command: this.command,
+			content: this.content
+		} = parseMessage(this.message, prefix));
 	}
 }
