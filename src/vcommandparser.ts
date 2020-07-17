25fb4faaeae4c205e23f9ffd4b273b918ac4b764
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2020-07-17 14:04:12 -0400

    Add JSDoc to public facing api

--- src/vcommandparser.ts
+++ src/vcommandparser.ts
@@ -9,12 +9,42 @@ export default class VCommandParser {
 	static readonly DEFAULT_COMMAND_PREFIX = '!';
 	static readonly DEFAULT_OPTION_PREFIX = '-';
 	
+	/**
+	 * This function parses a string and gets the component if the string is in the format `{PREFIX}{COMMAND} [{content}] [{OPTION_PREFIX}{OPTION} [{OPTION_CONTENT}]]` (where anything between `[]` is optional).
+	 *
+	 * This function lazily parses the message, meaning only the command is processed.
+	 * To process options, you can either use the `parsed.doParseOptions()` function or set the option definitions using `parsed.setOptionDefinitions(definitions)`.
+	 *
+	 * @param message String that potentially contains a command.
+	 * @param commandPrefix String that will be used for the `{PREFIX}` variable, in the format given above.
+	 * 	Defaults to `VCommandParser.DEFAULT_COMMAND_PREFIX` (`!`).
+	 * @param optionPrefix String that will be used for the `{OPTION_PREFIX}` variable, in the format given above.
+	 * 	Needs to be part of the `OptionPrefix` type.
+	 * 	Defaults to `VCommandParser.DEFAULT_OPTION_PREFIX` (`-`).
+	 *
+	 * @returns `VLazyParsedCommand` instance containing the results of the parsing.
+	 */
 	static parseLazy(message: string, commandPrefix = VCommandParser.DEFAULT_COMMAND_PREFIX, optionPrefix: OptionPrefix = VCommandParser.DEFAULT_OPTION_PREFIX): VLazyParsedCommand {
 		const parsedMessage = parseMessage(message, commandPrefix, optionPrefix, undefined, true);
 		
 		return new VLazyParsedCommand(message, commandPrefix, optionPrefix, parsedMessage);
 	}
 	
+	/**
+	 * This function parses a string and gets the component if the string is in the format `{PREFIX}{COMMAND} [{content}] [{OPTION_PREFIX}{OPTION} [{OPTION_CONTENT}]]` (where anything between `[]` is optional).
+	 *
+	 * If you didn't use the `optionDefinitions` param, you can set the option definitions afterward using `parsed.setOptionDefinitions(definitions)`.
+	 *
+	 * @param message String that potentially contains a command.
+	 * @param commandPrefix String that will be used for the `{PREFIX}` variable, in the format given above.
+	 * 	Defaults to `VCommandParser.DEFAULT_COMMAND_PREFIX` (`!`).
+	 * @param optionPrefix String that will be used for the `{OPTION_PREFIX}` variable, in the format given above.
+	 * 	Needs to be part of the `OptionPrefix` type.
+	 * 	Defaults to `VCommandParser.DEFAULT_OPTION_PREFIX` (`-`).
+	 * @param optionDefinitions Array of option definitions that tells the parser how to map options and tweak some behaviors, such as if the option accepts content.
+	 *
+	 * @returns `VParsedCommand` instance containing the results of the parsing.
+	 */
 	static parse(message: string, commandPrefix = VCommandParser.DEFAULT_COMMAND_PREFIX, optionPrefix: OptionPrefix = VCommandParser.DEFAULT_OPTION_PREFIX, optionDefinitions?: OptionDef[]): VParsedCommand {
 		const parsedMessage = parseMessage(message, commandPrefix, optionPrefix, optionDefinitions);
 		
@@ -55,6 +85,15 @@ export class VParsedCommand {
 		} = parsedMessage);
 	}
 	
+	/**
+	 * Retrieves an option from the list of options.
+	 *
+	 * @param search There is two ways this function will search for an option :
+	 * - If the value provided is a string, it will return the option that contains the value in its definition calls, and if there is no definition for the option, it will check if the option name matches the search value provided.
+	 * 	Otherwise, it will return `undefined`.
+	 * - If the value is a number, it will return the option that has the position that matches the search value.
+	 * 	Otherwise, it will return `undefined`.
+	 */
 	getOption(search: string | number): MessageOption | undefined {
 		return this.options?.find(option => {
 			if (typeof search == 'string') {
@@ -65,6 +104,11 @@ export class VParsedCommand {
 		});
 	}
 	
+	/**
+	 * Sets the option definitions, even if the message has already been parsed, and updates the properties to reflect the given definitions.
+	 *
+	 * @param optionDefinitions Array of option definitions that tells the parser how to map options and tweak some behaviors, such as if the option accepts content.
+	 */
 	setOptionDefinitions(optionDefinitions: OptionDef[]): void {
 		this.doExtractOptions(optionDefinitions);
 	}
@@ -88,6 +132,11 @@ export class VLazyParsedCommand extends VParsedCommand {
 		super(message, commandPrefix, optionPrefix, parsedMessage, optionDefinitions);
 	}
 	
+	/**
+	 * This function parses the full message to extract the options from the content of the message.
+	 *
+	 * This has no value if you called the `setOptionDefinitions` function before.
+	 */
 	doParseOptions(): void {
 		this.doExtractOptions();
 	}
